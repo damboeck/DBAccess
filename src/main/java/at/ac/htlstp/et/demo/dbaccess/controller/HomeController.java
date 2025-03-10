c8bbc7e03b0278697b5bc6e493c14af222e173ea
Werner Damboeck <werner.damboeck@htlstp.ac.at>
2025-03-10 16:39:37 +0100

    1.Stunde

--- src/main/java/at/ac/htlstp/et/demo/dbaccess/controller/HomeController.java
+++ src/main/java/at/ac/htlstp/et/demo/dbaccess/controller/HomeController.java
@@ -1,4 +1,21 @@
 package at.ac.htlstp.et.demo.dbaccess.controller;
 
+import at.ac.htlstp.et.demo.dbaccess.service.MysqlService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class HomeController {
+
+    @Autowired private MysqlService mysqlService;
+
+    @RequestMapping("/")
+    public String home(Model model) {
+        model.addAttribute("ct",mysqlService.teste());
+        model.addAttribute("msg","Hello World");
+        return "home";
+    }
 }
